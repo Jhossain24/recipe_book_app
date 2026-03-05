b7086f5d63b74ba7eab2ecc60d2f142cc250fb04
Joshoua R Hossain <jhossain4@student.gsu.edu>
2026-03-04 23:43:27 -0500

    Final changes to the detail screen to show all recipes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,60 @@
-// lib/screens/home_screen.dart
-
 import 'package:flutter/material.dart';
+import '../data/recipes_data.dart';
+import 'details_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-        title: const Text('Recipe Book'),
-
-      body: Center(
-        child: Text('Home Screen '),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('The Greatest Recipe Book Ever'),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: sampleRecipes.length,
+        itemBuilder: (context, index) {
+          final recipe = sampleRecipes[index];
+          return Card(
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: ListTile(
+              contentPadding: const EdgeInsets.all(8),
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset(
+                  recipe.imagePath,
+                  width: 60,
+                  height: 60,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) {
+                    return Container(
+                      width: 60,
+                      height: 60,
+                      color: Colors.grey[300],
+                      child: const Icon(Icons.broken_image),
+                    );
+                  },
+                ),
+              ),
+              title: Text(
+                recipe.name,
+                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+              onTap: () {
+                // For navigating to details screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailsScreen(recipe: recipe),
+                  ),
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
